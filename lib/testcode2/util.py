7a1ea92443d6a23452f3df002969b16a28be7449
James Spencer <j.spencer@imperial.ac.uk>
2012-04-01 20:41:08 +0100

    util functions: try_floatify and extract_tagged_data.
    try_floatify: convert a value to a float if possible.
    extract_tagged_data: parse a data file for lines marked with a suitable tag.

--- lib/testcode2/util.py
+++ lib/testcode2/util.py
@@ -1,5 +1,10 @@
 '''Utility functions.'''
 
+import os.path
+import re
+
+import testcode2.exceptions as exceptions
+
 def testcode_filename(stem, file_id, inp, args):
     '''Construct filename in testcode format.'''
     filename = '%s.%s' % (stem, file_id)
@@ -9,3 +14,48 @@ def testcode_filename(stem, file_id, inp, args):
         args_string = args.replace(' ','_')
         filename = '%s.args=%s' % (filename, args_string)
     return filename
+
+def try_floatify(val):
+    '''Convert val to a float if possible.'''
+    try:
+        return float(val)
+    except ValueError:
+        return val
+
+def extract_tagged_data(data_tag, filename):
+    '''Extract data from lines marked by the data_tag in filename.'''
+    if not os.path.exists(filename):
+        err = 'Cannot extract data: file %s does not exist.' % (filename)
+        raise exceptions.RunError(err)
+    data_file = open(filename)
+    # Data tag is the first non-space character in the line.
+    # e.g. extract data from lines:
+    # data_tag      Energy:    1.256743 a.u.
+    data_tag_regex = re.compile('^ *%s' % (re.escape(data_tag)))
+    data = {}
+    for line in data_file.readlines():
+        if data_tag_regex.match(line):
+            # This is a line containing info to be tested.
+            words = line.split()
+            key = []
+            # name of data is string after the data_tag and preceeding the
+            # (numerical) data.  only use the first number in the line, with
+            # the key taken from all proceeding information.
+            for word in words[1:]:
+                val = try_floatify(word)
+                if val != word:
+                    break
+                else:
+                    key.append(word)
+            if key[-1] in ("=",':'):
+                key.pop()
+            key = '_'.join(key)
+            if key[-1] in ("=",':'):
+                key = key[:-1]
+            if not key:
+                key = 'data'
+            if key in data:
+                data[key].append(val)
+            else:
+                data[key] = [val]
+    return [data]
