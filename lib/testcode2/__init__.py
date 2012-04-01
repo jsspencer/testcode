018d7af60f6d9709cdc5cdd947537dbf521dfef2
James Spencer <j.spencer@imperial.ac.uk>
2012-04-01 20:42:58 +0100

    Formatting.

--- lib/testcode2/__init__.py
+++ lib/testcode2/__init__.py
@@ -28,7 +28,8 @@ class TestProgram:
         # Running
         self.exe = exe
         self.test_id = test_id
-        self.run_cmd_template = 'tc.program tc.args tc.input > tc.output 2> tc.error'
+        self.run_cmd_template = ('tc.program tc.args tc.input > '
+                                                    'tc.output 2> tc.error')
         self.launch_parallel = None
         self.submit_template = None
 
