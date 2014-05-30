jsspencer/testcode

commit d7c10acd4d13577115f6324f088b0f4baa380477

Allow multiple benchmarks to be specified.
The first benchmark in the list in the userconfig file which exists for a given test is
used.  For now, the benchmark command-line option can still only be used to select
a single benchmark.  Need some experience to see if enabling that option to specify
alternate benchmark files would be useful.

bin/testcode.py
@@ -94,7 +94,7 @@ tests: list of selected tests.
     # Set benchmark if required.
     if benchmark:
         for key in test_programs:
-            test_programs[key].benchmark = benchmark
+            test_programs[key].benchmark = [benchmark]
 
     try:
         (tests, test_categories) = testcode2.config.parse_jobconfig(
@@ -598,7 +598,11 @@ verbose: level of verbosity in output (no output if <1).
                 print('Using executable: %s.' % (exe))
         # All tests use the same test_id and benchmark.
         print('Test id: %s.' % (tests[0].test_program.test_id))
-        print('Benchmark: %s.' % (tests[0].test_program.benchmark))
+        if len(tests[0].test_program.benchmark) > 1:
+            benchmark_ids = ', '.join(tests[0].test_program.benchmark)
+            print('Benchmarks: %s.' % (benchmark_ids))
+        else:
+            print('Benchmark: %s.' % (tests[0].test_program.benchmark[0]))
         print('')
 
 def end_status(tests, not_checked=0, verbose=1, final=True):

lib/testcode2/__init__.py
@@ -121,12 +121,11 @@ class TestProgram:
         cmd = cmd.replace('tc.nprocs', str(nprocs))
         return cmd
 
-    def extract_cmd(self, input_file, args):
+    def extract_cmd(self, path, input_file, args):
         '''Create extraction command(s).'''
         test_file = util.testcode_filename(FILESTEM['test'], self.test_id,
                 input_file, args)
-        bench_file = util.testcode_filename(FILESTEM['benchmark'],
-                self.benchmark, input_file, args)
+        bench_file = self.select_benchmark_file(path, input_file, args)
         cmd = self.extract_cmd_template
         cmd = cmd.replace('tc.extract', pipes.quote(self.extract_program))
         cmd = cmd.replace('tc.args', self.extract_args)
@@ -151,6 +150,23 @@ class TestProgram:
         cmd = cmd.replace('tc.args', self.skip_args)
         cmd = cmd.replace('tc.test', pipes.quote(test_file))
         return cmd
+
+    def select_benchmark_file(self, path, input_file, args):
+        '''Find the first benchmark file out of all benchmark IDs which exists.'''
+
+        benchmark = None
+        benchmarks = []
+        for bench_id in self.benchmark:
+            benchfile = util.testcode_filename(FILESTEM['benchmark'], bench_id,
+                    input_file, args)
+            benchmarks.append(benchfile)
+            if os.path.exists(os.path.join(path, benchfile)):
+                benchmark = benchfile
+                break
+        if not benchmark:
+            err = 'No benchmark found in %s.  Checked for: %s.'
+            raise exceptions.TestCodeError(err % (path, ', '.join(benchmarks)))
+        return benchmark
 
 class Test:
     '''Store and execute a test.'''
@@ -426,7 +442,7 @@ first, during initialisation.'''
                         msg = '\n'.join((msg, data_table))
                     else:
                         msg = data_table
-        except exceptions.AnalysisError:
+        except (exceptions.AnalysisError, exceptions.TestCodeError):
             if msg.strip():
                 msg = '%s\n%s' % (msg, sys.exc_info()[1])
             else:
@@ -473,7 +489,7 @@ first, during initialisation.'''
         '''Run user-supplied verifier script.
 
 Assume function is executed in self.path.'''
-        verify_cmd = self.test_program.extract_cmd(input_file, args)[0]
+        verify_cmd, = self.test_program.extract_cmd(self.path, input_file, args)
         try:
             if verbose > 2:
                 print('Analysing test using %s in %s.' %
@@ -503,8 +519,7 @@ Assume function is executed in self.path.'''
         if tp_ptr.data_tag:
             # Using internal data extraction function.
             data_files = [
-                    util.testcode_filename(FILESTEM['benchmark'],
-                            tp_ptr.benchmark, input_file, args),
+                    tp_ptr.select_benchmark_file(self.path, input_file, args),
                     util.testcode_filename(FILESTEM['test'],
                             tp_ptr.test_id, input_file, args),
                          ]
@@ -516,7 +531,7 @@ Assume function is executed in self.path.'''
         else:
             # Using external data extraction script.
             # Get extraction commands.
-            extract_cmds = self.test_program.extract_cmd(input_file, args)
+            extract_cmds = tp_ptr.extract_cmd(self.path, input_file, args)
 
             # Extract data.
             outputs = []

lib/testcode2/config.py
@@ -88,6 +88,8 @@ config_file: location of the userconfig file, either relative or absolute.'''
                 (parse_tolerance_tuple(item)
                      for item in eval_nested_tuple(user_options['tolerance']))
                                         )
+        if user_options['benchmark']:
+            user_options['benchmark'] = user_options['benchmark'].split()
     else:
         raise exceptions.TestCodeError(
                 'user section in userconfig does not exist.'

commit 4495a3658fe0079657300a8690aa3702f75fc1f7

Fix benchmark detection in diff command. .
The diff command did not know the change in benchmark format to allow for multiple
benchmark IDs.

Bug introduced in d7c10acd4d13577115f6324f088b0f4baa380477.

bin/testcode.py
@@ -468,21 +468,29 @@ verbose: level of verbosity in output.
         cwd = os.getcwd()
         os.chdir(test.path)
         for (inp, args) in test.inputs_args:
-            benchmark = testcode2.util.testcode_filename(
-                    testcode2.FILESTEM['benchmark'],
-                    test.test_program.benchmark, inp, args
-                    )
+            have_benchmark = True
+            try:
+                benchmark = test.test_program.select_benchmark_file(
+                        test.path, inp, args
+                        )
+            except testcode2.exceptions.TestCodeError:
+                err = sys.exc_info()[1]
+                have_benchmark = False
             test_file = testcode2.util.testcode_filename(
                     testcode2.FILESTEM['test'],
                     test.test_program.test_id, inp, args
                     )
-            if verbose > 0:
-                print('Diffing %s and %s in %s.' % (benchmark, test_file,
-                    test.path))
-            if not os.path.exists(test_file) or not os.path.exists(benchmark):
+            if not os.path.exists(test_file):
                 if verbose > 0:
-                    print('Skipping diff: %s does not exist.' % test_file)
+                    print('Skipping diff with %s in %s: %s does not exist.'
+                            % (benchmark, test.path, test_file))
+            elif not have_benchmark:
+                if verbose > 0:
+                    print('Skipping diff with %s. %s' % (test.path, err))
             else:
+                if verbose > 0:
+                    print('Diffing %s and %s in %s.' %
+                            (benchmark, test_file, test.path))
                 diff_cmd = '%s %s %s' % (diff_program, benchmark, test_file)
                 diff_popen = subprocess.Popen(diff_cmd, shell=True)
                 diff_popen.wait()